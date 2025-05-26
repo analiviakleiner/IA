40213eb28654fe3fcad21d9688caa828c0ad55b3
analiviakleiner <141344234+analiviakleiner@users.noreply.github.com>
2025-05-26 13:36:35 +0000

    ok

--- script.js
+++ script.js
@@ -3,21 +3,60 @@ const caixaPerguntas = document.querySelector("caixa-perguntas");
 const caixaAlternativas = document.querySelector("caixa-alternativas");
 const caixaResultado = document.querySelector("caixa-resultado");
 const textoResultado = document.querySelector("texto-resultado");
-const perguntas = [
+const perguntas = 
+[
     {
         enunciado: "Pergunta 1",
-        alternativas: ["Alternativa1","Alternativa2"]
+        alternativas: 
+        [
+            {
+            texto:"texto aternativa 1", 
+            afirmação:"afirmacao 1"
+            },
+        
+            {
+            texto:"texto aternativa 2", 
+            afirmação:"afirmacao 2"
+            },
+        ]
     },
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa1","Alternativa2"]
+      enunciado: "Pergunta 2",
+        alternativas: 
+        [  
+            {
+            texto:"texto aternativa 1", 
+            afirmação:"afirmacao 1"
+            },
+        
+            {
+            texto:"texto aternativa 2", 
+            afirmação:"afirmacao 2"
+            },
+        ]  
+    }
+    {
+      enunciado: "Pergunta 3",
+        alternativas: 
+        [  
+            {
+            texto:"texto aternativa 1", 
+            afirmação:"afirmacao 1"
+            },
+        
+            {
+            texto:"texto aternativa 2", 
+            afirmação:"afirmacao 2"
+            },
+        ]  
     }
 ];
 
 let atual = 0;
 let perguntaAtual;
 
-function mostrarPergunta(){
+function mostrarPergunta()
+{
     perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
