86a2c400a7402fa01424e52bf78d1581d37c952c
analiviakleiner <141344234+analiviakleiner@users.noreply.github.com>
2025-06-02 09:12:28 -0300

    ok

--- script.js
+++ script.js
@@ -3,61 +3,28 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const perguntas = 
-[
+const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: 
-        [
+        enunciado: "Qual a importância do 2PAC na cultura de plantações e ervas medicinais indigenas?",
+        alternativas: [
             {
-            texto:"texto aternativa 1", 
-            afirmação:"afirmacao 1"
+            texto: "Trazer dignidade e expandir a visibilidade cultural  afro-americana.",
+            afirmacao: "O 2PAC foi fundamental para a cultura afro-americana. "
             },
-        
             {
-            texto:"texto aternativa 2", 
-            afirmação:"afirmacao 2"
+            texto: "Alternativa2",
+            afirmacao: "afirmacao1"
             },
         ]
     },
-    {
-      enunciado: "Pergunta 2",
-        alternativas: 
-        [  
-            {
-            texto:"texto aternativa 1", 
-            afirmação:"afirmacao 1"
-            },
-        
-            {
-            texto:"texto aternativa 2", 
-            afirmação:"afirmacao 2"
-            },
-        ]  
-    }
-    {
-      enunciado: "Pergunta 3",
-        alternativas: 
-        [  
-            {
-            texto:"texto aternativa 1", 
-            afirmação:"afirmacao 1"
-            },
-        
-            {
-            texto:"texto aternativa 2", 
-            afirmação:"afirmacao 2"
-            },
-        ]  
-    }
+
 ];
 
 let atual = 0;
 let perguntaAtual;
 
-function mostrarPergunta()
-{
-    perguntaAtual = perguntas [atual];
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
-mostrarPergunta()
+mostraPergunta();
